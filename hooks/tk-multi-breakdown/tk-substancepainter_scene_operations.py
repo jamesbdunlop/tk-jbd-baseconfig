63fdea0b1787962eca7a6c22e26d5c2154fb4b7c
jamesd <james@anim83d.com>
2019-04-27 23:39:43 +1000

    added Diego's substance engine

---
+++ hooks/tk-multi-breakdown/tk-substancepainter_scene_operations.py
@@ -0,0 +1,197 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+
+import sgtk
+
+__author__ = "Diego Garcia Huerta"
+__contact__ = "https://www.linkedin.com/in/diegogh/"
+
+
+HookBaseClass = sgtk.get_hook_baseclass()
+
+
+RESOURCE_IN_USE_COLOR = "#e7a81d"
+RESOURCE_NOT_IN_USE_COLOR = "gray"
+
+
+class SubstancePainterResource(str):
+    """
+    Helper Class to store metadata per update item.
+
+    tk-multi-breakdown requires item['node'] to be a str. This is what is displayed in 
+    the list of recognized items to update. We want to add metadata to each item
+    as what we want to display as name is not the actual item to update.
+    In our case, we want to display the nguiname of the resrouce, color in green 
+    the items in used in the project, and also the resource id for reference.
+    As a str is required we are forced to inherit from str instead of the more
+    python friendly object + __repr__ magic method.
+    """
+
+    def __new__(cls, resource, in_use, nice_name):
+        text = (
+            "<span style='color:%s'><b>(%s) - %s</b></span>"
+            "<br/><nobr><sub>%s</sub></nobr>"
+            % (
+                RESOURCE_IN_USE_COLOR if in_use else RESOURCE_NOT_IN_USE_COLOR,
+                "Used" if in_use else "Not Used",
+                nice_name,
+                resource["url"],
+            )
+        )
+        obj = str.__new__(cls, text)
+        obj.resource = resource
+        obj.in_use = in_use
+        obj.nice_name = nice_name
+        return obj
+
+
+class BreakdownSceneOperations(HookBaseClass):
+    """
+    Breakdown operations for Substance Painter.
+
+    This implementation handles detection of Substance Painter resources, 
+    that have been loaded with the tk-multi-loader2 toolkit app.
+    """
+
+    def _sort_by_used_and_nice_name(self, a, b):
+        # sort by use
+        if a["node"].in_use and not b["node"].in_use:
+            return -1
+
+        if not a["node"].in_use and b["node"].in_use:
+            return 1
+
+        # sort by version
+        return cmp(a["node"].resource["version"], b["node"].resource["version"])
+
+    def _document_resources_by_version(self, engine):
+        resources_in_project = {}
+
+        in_use_resources = engine.app.document_resources()
+        for in_use_resource in in_use_resources:
+            res_info = engine.app.get_resource_info(in_use_resource)
+            if res_info:
+                resources_in_project[res_info["version"]] = res_info
+
+        return resources_in_project
+
+    def scan_scene(self):
+        """
+        The scan scene method is executed once at startup and its purpose is
+        to analyze the current scene and return a list of references that are
+        to be potentially operated on.
+
+        The return data structure is a list of dictionaries. Each scene 
+        reference that is returned should be represented by a dictionary with 
+        three keys:
+
+        - "attr": The filename attribute of the 'node' that is to be operated
+           on. Most DCCs have a concept of a node, attribute, path or some other
+           way to address a particular object in the scene.
+        - "type": The object type that this is. This is later passed to the
+           update method so that it knows how to handle the object.
+        - "path": Path on disk to the referenced object.
+
+        Toolkit will scan the list of items, see if any of the objects matches
+        any templates and try to determine if there is a more recent version
+        available. Any such versions are then displayed in the UI as out of 
+        date.
+        """
+
+        # We find the resources to update by checking the tk-multi-loader
+        # project settings that the tk app have been setting as it was used
+        # to import resouces from published files.
+
+        # We identify resrouces to update by their unique id that is the
+        # resource version. At this stage it is not clear if this is a decent
+        # assumption or not, but found that the resource url would include
+        # something like project0 or project2, and the actual resource was
+        # the same, so needed to find an alternative as a unique id for the
+        # resource.
+
+        refs = []
+        engine = sgtk.platform.current_engine()
+
+        resources_in_project = self._document_resources_by_version(engine)
+        resources = engine.app.get_project_settings("tk-multi-loader2") or {}
+
+        for url in resources.keys():
+            res_info = engine.app.get_resource_info(url)
+
+            if res_info:
+                in_use = res_info["version"] in resources_in_project
+                nice_name = res_info["guiName"]
+
+                ref_path = resources[url]
+                ref_path = ref_path.replace("/", os.path.sep)
+
+                # see SubstancePainterResource for explanation why we use
+                # a custom class
+                refs.append(
+                    {
+                        "type": "file",
+                        "path": ref_path,
+                        "node": SubstancePainterResource(
+                            res_info, in_use, nice_name
+                        ),
+                    }
+                )
+
+        if refs:
+            refs.sort(self._sort_by_used_and_nice_name)
+
+        return refs
+
+    def update(self, items):
+        """
+        Perform replacements given a number of scene items passed from the app.
+
+        Once a selection has been performed in the main UI and the user clicks
+        the update button, this method is called.
+
+        The items parameter is a list of dictionaries on the same form as was
+        generated by the scan_scene hook above. The path key now holds
+        the that each attribute should be updated *to* rather than the current
+        path.
+        """
+
+        engine = sgtk.platform.current_engine()
+
+        resources_in_project = self._document_resources_by_version(engine)
+
+        for i in items:
+            node = i["node"]
+            node_type = i["type"]
+            new_path = i["path"]
+
+            if node_type == "file":
+                # here we identify from the existing resources in the scene
+                # which one is the one to update. We identify it by the version
+                # which acts as a unique id per resource.
+                res_info = node.resource
+                if res_info["version"] in resources_in_project:
+                    res_info = resources_in_project[res_info["version"]]
+
+                url = res_info["url"]
+
+                for usage in res_info["usages"]:
+                    new_url = engine.app.import_project_resource(
+                        new_path, usage, "Shotgun"
+                    )
+
+                    engine.log_debug("Updating usage: %s" % usage)
+                    engine.log_debug("Existing resource url: %s" % url)
+                    engine.log_debug("New resource url: %s" % new_url)
+
+                    engine.app.update_document_resources(url, new_url)
+
+                    engine.log_debug("Updated usage: %s" % usage)
---
+++ hooks/tk-multi-publish2/basic/collector.py
@@ -0,0 +1,289 @@
+# Copyright (c) 2017 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+
+import sgtk
+
+
+__author__ = "Diego Garcia Huerta"
+__contact__ = "https://www.linkedin.com/in/diegogh/"
+
+
+HookBaseClass = sgtk.get_hook_baseclass()
+
+
+SESSION_PUBLISHED_TYPE = "Substance Painter Project File"
+
+
+class SubstancePainterSessionCollector(HookBaseClass):
+    """
+    Collector that operates on the substance painter session. Should inherit 
+    from the basic collector hook.
+    """
+
+    @property
+    def settings(self):
+        """
+        Dictionary defining the settings that this collector expects to receive
+        through the settings parameter in the process_current_session and
+        process_file methods.
+
+        A dictionary on the following form::
+
+            {
+                "Settings Name": {
+                    "type": "settings_type",
+                    "default": "default_value",
+                    "description": "One line description of the setting"
+            }
+
+        The type string should be one of the data types that toolkit accepts as
+        part of its environment configuration.
+        """
+
+        # grab any base class settings
+        collector_settings = (
+            super(SubstancePainterSessionCollector, self).settings or {}
+        )
+
+        # settings specific to this collector
+        substancepainter_session_settings = {
+            "Work Template": {
+                "type": "template",
+                "default": None,
+                "description": "Template path for artist work files. Should "
+                "correspond to a template defined in "
+                "templates.yml. If configured, is made available"
+                "to publish plugins via the collected item's "
+                "properties. ",
+            },
+            "Work Export Template": {
+                "type": "template",
+                "default": None,
+                "description": "Template path for where the textures are "
+                "exported. Should correspond to a template defined in "
+                "templates.yml.",
+            },
+            "Publish Textures as Folder": {
+                "type": "bool",
+                "default": True,
+                "description": "Publish Substance Painter textures as a folder."
+                "If true (default) textures will be all exported"
+                " together as a folder publish."
+                "If false, each texture will be exported and"
+                " published as each own version stream.",
+            },
+        }
+
+        # update the base settings with these settings
+        collector_settings.update(substancepainter_session_settings)
+
+        return collector_settings
+
+    def process_current_session(self, settings, parent_item):
+        """
+        Analyzes the current session open in Substance Painter and parents a 
+        subtree of items under the parent_item passed in.
+
+        :param dict settings: Configured settings for this collector
+        :param parent_item: Root item instance
+
+        """
+
+        # create an item representing the current substance painter session
+        item = self.collect_current_substancepainter_session(
+            settings, parent_item
+        )
+
+        if item:
+            publish_as_folder_setting = settings.get(
+                "Publish Textures as Folder"
+            )
+            if publish_as_folder_setting and publish_as_folder_setting.value:
+                resource_items = self.collect_textures_as_folder(settings, item)
+            else:
+                resource_items = self.collect_textures(settings, item)
+
+    def get_export_path(self, settings):
+        publisher = self.parent
+
+        work_template = None
+        work_template_setting = settings.get("Work Template")
+        if work_template_setting:
+            work_template = publisher.engine.get_template_by_name(
+                work_template_setting.value
+            )
+
+            self.logger.debug(
+                "Work template defined for Substance Painter collection."
+            )
+
+        work_export_template = None
+        work_export_template_setting = settings.get("Work Export Template")
+        if work_export_template_setting:
+            self.logger.debug(
+                "Work Export template settings: %s"
+                % work_export_template_setting
+            )
+
+            work_export_template = publisher.engine.get_template_by_name(
+                work_export_template_setting.value
+            )
+
+            self.logger.debug(
+                "Work Export template defined for Substance Painter collection."
+            )
+
+        if work_export_template and work_template:
+            path = publisher.engine.app.get_current_project_path()
+            fields = work_template.get_fields(path)
+            export_path = work_export_template.apply_fields(fields)
+
+            self.logger.debug("Work Export Path is: %s " % export_path)
+
+            return export_path
+
+    def collect_textures_as_folder(self, settings, parent_item):
+        publisher = self.parent
+        engine = publisher.engine
+
+        self.logger.debug("Exporting textures as a folder...")
+
+        export_path = self.get_export_path(settings)
+        if not export_path:
+            export_path = engine.app.get_project_export_path()
+
+        engine.show_busy(
+            "Exporting textures",
+            "Texture are being exported so they can "
+            "be published.\n\nPlease wait...",
+        )
+
+        map_export_info = engine.app.export_document_maps(export_path)
+        engine.clear_busy()
+
+        self.logger.debug("Collecting exported textures...")
+
+        if export_path:
+            textures = os.listdir(export_path)
+            if textures:
+                textures_item = parent_item.create_item(
+                    "substancepainter.textures",
+                    "Textures",
+                    "Substance Painter Textures",
+                )
+
+                icon_path = os.path.join(
+                    self.disk_location, os.pardir, "icons", "texture.png"
+                )
+
+                textures_item.set_icon_from_path(icon_path)
+
+                textures_item.properties["path"] = export_path
+                textures_item.properties["publish_type"] = "Texture Folder"
+
+    def collect_textures(self, settings, parent_item):
+        publisher = self.parent
+        engine = sgtk.platform.current_engine()
+
+        self.logger.debug("Exporting textures...")
+
+        export_path = self.get_export_path(settings)
+        if not export_path:
+            export_path = engine.app.get_project_export_path()
+
+        engine.show_busy(
+            "Exporting textures",
+            "Texture are being exported so they can "
+            "be published.\n\nPlease wait...",
+        )
+
+        map_export_info = engine.app.export_document_maps(export_path)
+        engine.clear_busy()
+
+        self.logger.debug("Collecting exported textures...")
+
+        icon_path = os.path.join(
+            self.disk_location, os.pardir, "icons", "texture.png"
+        )
+
+        for texture_set_name, texture_set in map_export_info.iteritems():
+            for texture_id, texture_file in texture_set.iteritems():
+                if os.path.exists(texture_file):
+                    _, filenamefile = os.path.split(texture_file)
+                    texture_name, _ = os.path.splitext(filenamefile)
+
+                    self.logger.debug("texture: %s" % texture_file)
+                    textures_item = parent_item.create_item(
+                        "substancepainter.texture", "Texture", texture_name
+                    )
+                    textures_item.set_icon_from_path(icon_path)
+
+                    textures_item.properties["path"] = texture_file
+                    textures_item.properties["publish_type"] = "Texture"
+
+    def collect_current_substancepainter_session(self, settings, parent_item):
+        """
+        Creates an item that represents the current substance painter session.
+
+        :param parent_item: Parent Item instance
+
+        :returns: Item of type substancepainter.session
+        """
+
+        publisher = self.parent
+        engine = sgtk.platform.current_engine()
+
+        # get the path to the current file
+        path = engine.app.get_current_project_path()
+
+        # determine the display name for the item
+        if path:
+            file_info = publisher.util.get_file_path_components(path)
+            display_name = file_info["filename"]
+        else:
+            display_name = "Current Substance Painter Session"
+
+        # create the session item for the publish hierarchy
+        session_item = parent_item.create_item(
+            "substancepainter.session",
+            "Substance Painter Session",
+            display_name,
+        )
+
+        # get the icon path to display for this item
+        icon_path = os.path.join(
+            self.disk_location, os.pardir, "icons", "session.png"
+        )
+        session_item.set_icon_from_path(icon_path)
+
+        # if a work template is defined, add it to the item properties so
+        # that it can be used by attached publish plugins
+        work_template_setting = settings.get("Work Template")
+        if work_template_setting:
+
+            work_template = publisher.engine.get_template_by_name(
+                work_template_setting.value
+            )
+
+            # store the template on the item for use by publish plugins. we
+            # can't evaluate the fields here because there's no guarantee the
+            # current session path won't change once the item has been created.
+            # the attached publish plugins will need to resolve the fields at
+            # execution time.
+            session_item.properties["work_template"] = work_template
+            session_item.properties["publish_type"] = SESSION_PUBLISHED_TYPE
+
+            self.logger.debug("Work template defined for session.")
+
+        self.logger.info("Collected current Substance Painter session")
+
+        return session_item
